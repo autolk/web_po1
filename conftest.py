e5e388ab1585b37c1b27cbf1288b76f7109afa0f
autolk <58027435+autolk@users.noreply.github.com>
2022-08-06 08:39:19 +0800

    修改注释

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
 # def pytest_runtest_setup(item):
 #     print('11111')
 
-# 修改编码格式
+# 修改编码格式/这个方法针对的是hook_fun.py中的方法
 def pytest_collection_modifyitems(session, config, items):
     print(items)
     for item in items:
+        print(item.name)
+        print(item._nodeid)
         item.name = item.name.encode('utf-8').decode('unicode-escape')
         item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
