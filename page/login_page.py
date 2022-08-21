ab72ed09f10cb8c4585592501a73388a94a2d353
autolk <58027435+autolk@users.noreply.github.com>
2022-08-21 08:54:25 +0800

    新增了一个文件，等着写装饰器吧

--- page/login_page.py
+++ page/login_page.py
@@ -2,17 +2,17 @@ from selenium import webdriver
 import yaml
 from page.home_page import HomePage
 from base.Base import Base
+import time
 class LoginPage(Base):
 
     def login(self):
 
-        with open('./datas/cookies.yaml','r')as f:
+        with open('C:/Users/Administrator/Desktop/huogewoci/web_po/datas/cookies.yaml','r')as f:
             cookies = yaml.safe_load(f)
 
         for cookie in cookies:
             self.driver.add_cookie(cookie)
         self.driver.get('https://work.weixin.qq.com/wework_admin/loginpage_wx?from=myhome_baidu')
-
         return HomePage(self.driver)
 
-# LoginPage().login()
+
