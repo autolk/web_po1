837735f734a057384e3b2424c30635c074ca1eb3
autolk <likun_picc@163.c0m>
2022-08-22 15:09:18 +0800

    修改目录问题

--- page/login_page.py
+++ page/login_page.py
@@ -7,13 +7,13 @@ class LoginPage(Base):
 
     def login(self):
 
-        with open('C:/Users/25238/Desktop/huo/web_po1/datas/cookies1.yaml','r')as f:
+        with open('../datas/cookies2.yaml','r')as f:
             cookies = yaml.safe_load(f)
 
         for cookie in cookies:
             self.driver.add_cookie(cookie)
         self.driver.get('https://work.weixin.qq.com/wework_admin/loginpage_wx?from=myhome_baidu')
-        time.sleep(25)
+        time.sleep(5)
         return HomePage(self.driver)
 
 
